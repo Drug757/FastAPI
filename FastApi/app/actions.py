24ee7d74203e6fa8f69dabcbbab8f1a30ae85a37
Drug757 <hgit07680@gmail.com>
2026-04-07 10:18:30 +0300

    Delete FastApi/app directory

--- FastApi/app/actions.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-# 3. BackgroundTasks: Имитация тяжелой операции (генерация отчета)
-async def generate_inventory_report(product_name: str):
-    await asyncio.sleep(5) # Имитация долгой работы
-    print(f"[REPORT] Отчет для товара '{product_name}' готов и отправлен в архив.")
-
-def calculate_sku(name: str, category: str) -> str:
-    return f"{category[:3].upper()}-{name[:3].upper()}-001"
